5cf1014b782ffa7073af843cc86b95580262133b
Etienne Robert <etiennerobert33@gmail.com>
2026-04-29 15:24:17 +0200

    refactor flake: add watch app, simplify forAllSystems

--- flake.nix
+++ flake.nix
@@ -4,47 +4,45 @@
   outputs = { nixpkgs, self }:
     let
       systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
-      forAllSystems = nixpkgs.lib.genAttrs systems;
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          tex = pkgs.texlive.combine {
+            inherit (pkgs.texlive) scheme-basic geometry hyperref enumitem titlesec parskip ec cm-super;
+          };
+        in
+        f pkgs tex);
     in
     {
-      packages = forAllSystems (system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-          tex = pkgs.texlive.combine {
-            inherit (pkgs.texlive) scheme-basic geometry hyperref enumitem titlesec parskip ec cm-super;
-          };
-          cv = pkgs.stdenvNoCC.mkDerivation {
-            name = "cv";
-            src = ./.;
-            buildInputs = [ tex ];
-            buildPhase = "pdflatex cv.tex";
-            installPhase = "install -Dm644 cv.pdf $out/cv.pdf";
-          };
-        in
-        { default = cv; });
+      packages = forAllSystems (pkgs: tex: {
+        default = pkgs.stdenvNoCC.mkDerivation {
+          name = "cv";
+          src = ./.;
+          buildInputs = [ tex ];
+          buildPhase = "pdflatex cv.tex";
+          installPhase = "install -Dm644 cv.pdf $out/cv.pdf";
+        };
+      });
 
-      apps = forAllSystems (system:
-        let pkgs = nixpkgs.legacyPackages.${system}; in
-        {
-          default = {
-            type = "app";
-            program = "${pkgs.writeShellScript "open-cv" ''
-              exec ${pkgs.xdg-utils}/bin/xdg-open ${self.packages.${system}.default}/cv.pdf
-            ''}";
-          };
-        });
+      apps = forAllSystems (pkgs: tex: {
+        default = {
+          type = "app";
+          program = "${pkgs.writeShellScript "open-cv" ''
+            exec ${pkgs.xdg-utils}/bin/xdg-open ${self.packages.${pkgs.system}.default}/cv.pdf
+          ''}";
+        };
+        watch = {
+          type = "app";
+          program = "${pkgs.writeShellScript "watch-cv" ''
+            echo cv.tex | ${pkgs.entr}/bin/entr ${tex}/bin/pdflatex cv.tex
+          ''}";
+        };
+      });
 
-      devShells = forAllSystems (system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-          tex = pkgs.texlive.combine {
-            inherit (pkgs.texlive) scheme-basic geometry hyperref enumitem titlesec parskip ec cm-super;
-          };
-        in
-        {
-          default = pkgs.mkShell {
-            packages = [ tex ];
-          };
-        });
+      devShells = forAllSystems (pkgs: tex: {
+        default = pkgs.mkShell {
+          packages = [ tex pkgs.entr ];
+        };
+      });
     };
 }
